9f36d103c4a2c1ea193244e912288083c0fd6d93
abirmeadows <abirmeadows@gmail.com>
2021-01-25 15:48:56 +0600

    Compiling typescript

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nuxt/types';
 import { BCMSMostConfig } from '@becomes/cms-most/types';
 
-const bcmsModule: Module<BCMSMostConfig> = function (moduleOptions) {
+const nuxtModule: Module<BCMSMostConfig> = function(moduleOptions) {
   const options: BCMSMostConfig = {
     ...this.options.bcms,
     ...moduleOptions,
   };
 
-  console.log(options);
+  this.nuxt.hook('ready', () => {
+    console.log('On pre init');
+    console.log('options', options);
+  });
 
-  // Use this, this.options, this.nuxt
-  // Use moduleOptions
+  this.nuxt.hook('generate:done', () => {
+    console.log('On post build');
+    console.log('options', options);
+  });
 };
 
-export default bcmsModule;
+export default nuxtModule;
 
 // REQUIRED if publishing the module as npm package
 // export const meta = require('./package.json')
