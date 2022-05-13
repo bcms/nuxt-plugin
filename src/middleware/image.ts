605eaaae896c1b26f4fee30e62fb50592a882801
Bbanez <banezprod@gmail.com>
2022-05-13 15:53:27 +0200

    Remove comments

--- src/middleware/image.ts
+++ src/middleware/image.ts
@@ -4,23 +4,10 @@ import { useBcmsMost } from '../module';
 
 export function createBcmsNuxtImageMiddleware(): ServerMiddleware {
   return async (req, res) => {
-    console.log('HETE')
     const most = useBcmsMost();
-    // try {
     const result = await most.imageProcessor.middlewareHelper(
       req.url as string,
     );
-    // const result: AxiosResponse<{
-    //   exist: boolean;
-    //   path: string;
-    //   mimetype: string;
-    //   fileName: string;
-    //   fileSize: number;
-    // }> = await axios({
-    //   url: `http://localhost:3001/api/bcms-images${req.url}`,
-    //   method: (req.method as Method) || 'GET',
-    //   headers: req.headers as { [name: string]: string },
-    // });
     if (result.exist) {
       res.statusCode = 200;
       res.setHeader('Content-Type', result.mimetype as string);
@@ -32,14 +19,6 @@ export function createBcmsNuxtImageMiddleware(): ServerMiddleware {
       res.statusCode = 404;
       res.end();
     }
-    // } catch (error) {
-    //   const err = error as AxiosError;
-    //   const rez = err.response as AxiosResponse;
-    //   res.statusCode = rez.status;
-    //   res.setHeader('Content-Type', 'application/json');
-    //   res.write(JSON.stringify(rez.data));
-    //   res.end();
-    // }
   };
 }
 
