5462a4ff116031c9f33402e504eca96511e036f4
Bbanez <banezprod@gmail.com>
2023-06-30 11:32:08 +0200

    fetch media info over http

--- src/runtime/middleware/image.ts
+++ src/runtime/middleware/image.ts
@@ -1,19 +1,36 @@
 import * as fs from 'fs';
 import { defineEventHandler } from 'h3';
-import { useBcmsMost } from '../../module';
+import axios from 'axios';
 
 export default defineEventHandler(async (event) => {
   const { req, res } = event.node;
-  const most = useBcmsMost();
-  const result = await most.imageProcessor.middlewareHelper(req.url as string);
-  if (result.exist) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', result.mimetype as string);
-    const readStream = fs.createReadStream(result.path as string);
-    readStream.pipe(res, {
-      end: true,
+  try {
+    const response = await axios({
+      url: `http://localhost:3001/api/bcms-images/${req.url}`,
+      method: 'GET',
     });
-  } else {
+    const result: {
+      exist: boolean;
+      path?: string | undefined;
+      mimetype?: string | undefined;
+      fileName?: string | undefined;
+      fileSize?: number | undefined;
+    } = response.data;
+    if (result.exist) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', result.mimetype as string);
+      const readStream = fs.createReadStream(result.path as string);
+      readStream.pipe(res, {
+        end: true,
+      });
+      return;
+    } else {
+      res.statusCode = 404;
+      res.end();
+      return;
+    }
+  } catch (error) {
+    console.error(error);
     res.statusCode = 404;
     res.end();
   }
